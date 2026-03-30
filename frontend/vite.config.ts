6acc65c612373b754f664299ac227664aedb00b6
vsv-noon <forvsn@gmail.com>
2026-03-30 21:35:34 +0700

    feat: add docker-compose

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -5,6 +5,14 @@ import react from '@vitejs/plugin-react';
 // https://vite.dev/config/
 const viteConfig = defineConfig({
   plugins: [react()],
+  server: {
+    watch: {
+      usePolling: true,
+    },
+    host: true, // Needed for the Docker Container port mapping to work correctly
+    strictPort: true,
+    port: 5173, // Default Vite port
+  },
 });
 
 const vitestConfig = defineVitestConfig({
